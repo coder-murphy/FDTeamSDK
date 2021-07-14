cbff309a0c215ef69519f2b395047e378ede4365
oopsReach <oopsReach@foxmail.com>
2021-07-14 21:11:51 +0800

    添加函数式编程扩展

---
+++ src/FDTeamSDK/FunctionProgramming/FPExtensions.cs
@@ -0,0 +1,29 @@
+﻿using FDSDK.GenericSupports.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FDSDK.FunctionProgramming
+{
+    /// <summary>
+    /// 函数式编程扩展
+    /// </summary>
+    public static class FPExtensions
+    {
+        /// <summary>
+        /// 运行switch语句
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="switchs"></param>
+        public static void Switch<T>(this IEnumerable<IFDSwitchLine<T>> switchs)
+        {
+            switchs.ForAll(x => {
+                if(x != null && x.Src != null && x.MyAction != null && x.Expression != null)
+                if (x.Expression(x.Src)) x.MyAction(x.Src);
+            });
+        }
+    }
+
+}
---
+++ src/FDTeamSDK/FunctionProgramming/IFDSwitchLine.cs
@@ -0,0 +1,30 @@
+﻿using FDSDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FDSDK.FunctionProgramming
+{
+    /// <summary>
+    /// switch行必备参数
+    /// </summary>
+    public interface IFDSwitchLine<T> : IFDSDKObjectBase
+    {
+        /// <summary>
+        /// 源数据
+        /// </summary>
+        T Src { get; set; }
+
+        /// <summary>
+        /// 判断表达式
+        /// </summary>
+        Func<T, bool> Expression { get; set; }
+
+        /// <summary>
+        /// 如果满足条件执行动作
+        /// </summary>
+        Action<T> MyAction { get; set; }
+    }
+}
